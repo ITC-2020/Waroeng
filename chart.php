39adab8659892ed52f069421003b90c1f1d185b8
rezapb2001 <rezapb2001@gmail.com>
2020-12-06 13:06:01 +0700

    chart

--- chart.php
+++ chart.php
@@ -29,90 +29,110 @@
         <div id="content" class="pt-4 pb-5">
             <div class="container">
                 <br>
-                <div class="item-chart row">
-                    <h6 class=" col-sm-4 pl-5 pt-4 text-secondary text-dark">ITEM DETAIL</h6>
-                    <h6 class=" col-sm-3 pl-5 pt-4 text-secondary text-dark text-center">PRICE</h6>
-                    <h6 class=" col-sm-3 pl-5 pt-4 text-secondary text-dark">QTY</h6>
-                    <h6 class=" col-sm-2 pl-5 pt-4 text-secondary text-dark"></h6>
-                </div>
                 <div class="chart-box d-flex flex-column ">
                     <div class="flex-row d-flex">
-                        <div class="item-chart p-1 border-top border-bottom">
-                            <div class="d-flex mb-2">
-                                <div class="chart-product d-flex">
-                                    <img src="./assets/img/shoes.png" alt="product image">
-                                    <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
-                                </div> <!-- end chart-product -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                                <div class="chart-quantity m-auto">
-                                    <button class="minus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                    <input class="qty-inpput" type="text" name="name" value="1" disabled>
-                                    <button class="plus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div> <!-- end chart-quantity -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                            </div> <!-- end d-flex -->
-                            <div class="d-flex mb-2">
-                                <div class="chart-product d-flex">
-                                    <img src="./assets/img/shoes.png" alt="product image">
-                                    <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
-                                </div> <!-- end chart-product -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                                <div class="chart-quantity m-auto">
-                                    <button class="minus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                    <input class="qty-inpput" type="text" name="name" value="1" disabled>
-                                    <button class="plus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div> <!-- end chart-quantity -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                            </div> <!-- end d-flex -->
-                            <div class="d-flex mb-2">
-                                <div class="chart-product d-flex">
-                                    <img src="./assets/img/shoes.png" alt="product image">
-                                    <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
-                                </div> <!-- end chart-product -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                                <div class="chart-quantity m-auto">
-                                    <button class="minus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-minus"></i>
-                                    </button>
-                                    <input class="qty-inpput" type="text" name="name" value="1" disabled>
-                                    <button class="plus-btn btn-quantity" type="button" name="button">
-                                        <i class="fa fa-plus"></i>
-                                    </button>
-                                </div> <!-- end chart-quantity -->
-                                <div class="chart-price h-100 m-auto">
-                                    <span class="text-secondary">Rp. 255.000</span>
-                                </div> <!-- end chart-price -->
-                            </div> <!-- end d-flex -->
+                        <div class="item-chart row">
+                            <div class="head-item row">
+                                <h6 class=" col-sm-5 pl-5 pt-4 text-secondary text-dark">ITEM DETAIL</h6>
+                                <h6 class=" col-sm-2 pl-4 pt-4 text-secondary text-dark">PRICE</h6>
+                                <h6 class=" col-sm-3 pl-5 pt-4 text-secondary text-dark">QTY</h6>
+                            </div>
+                            <div class=" item-in p-1 border-top border-bottom">
+                                <div class="d-flex mb-2">
+                                    <div class="chart-product d-flex">
+                                        <img src="./assets/img/shoes.png" alt="product image">
+                                        <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
+                                    </div> <!-- end chart-product -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                    <div class="chart-quantity m-auto">
+                                        <button class="minus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                        <input class="qty-inpput" type="text" name="name" value="1" disabled>
+                                        <button class="plus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div> <!-- end chart-quantity -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                </div> <!-- end d-flex -->
+                                <div class="d-flex mb-2">
+                                    <div class="chart-product d-flex">
+                                        <img src="./assets/img/shoes.png" alt="product image">
+                                        <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
+                                    </div> <!-- end chart-product -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                    <div class="chart-quantity m-auto">
+                                        <button class="minus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                        <input class="qty-inpput" type="text" name="name" value="1" disabled>
+                                        <button class="plus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div> <!-- end chart-quantity -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                </div> <!-- end d-flex -->
+                                <div class="d-flex mb-2">
+                                    <div class="chart-product d-flex">
+                                        <img src="./assets/img/shoes.png" alt="product image">
+                                        <span class="text-secondary mt-auto mb-auto"><a href="">Sepatu remaja kekinian</a></span>
+                                    </div> <!-- end chart-product -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                    <div class="chart-quantity m-auto">
+                                        <button class="minus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                        <input class="qty-inpput" type="text" name="name" value="1" disabled>
+                                        <button class="plus-btn btn-quantity" type="button" name="button">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div> <!-- end chart-quantity -->
+                                    <div class="chart-price h-100 m-auto">
+                                        <span class="text-secondary">Rp. 255.000</span>
+                                    </div> <!-- end chart-price -->
+                                </div> <!-- end d-flex -->
 
-                            <!-- Ini adalah untuk total bayar kurir -->
-                            <br>
+                                <!-- Ini adalah untuk total bayar kurir -->
+                                <br>
+                            </div> <!-- end item-in -->
                         </div> <!-- end item-chart -->
-                        <div class="discount-chart p-5">
-                            <img src="./assets/img/ad.png" alt="">
+                        <div class="discount-chart">
+                            <img src="./assets/img/ad.png" alt="" class="img-discount">
                         </div> <!-- end address-chart -->
                     </div> <!-- end flex-row -->
-                    <div class="d-flex justify-content-between p-5">
-                        <button class="btn btn-white">Back</button>
-                        <button class="btn btn-brown">Pay Rp.1.600.500</button>
-                    </div> <!-- end d-flex -->
+                    <div class="flex-row d-flex m-2">
+                        <div>
+                            <div class="input-group">
+                                <input type="code" class="form-control mr-2" placeholder="Coupon code">
+                                <button name="button" class="btn btn-secondary"> APPLY </button>
+                            </div> <!-- end coupon-code -->
+                        </div> <!-- end coupon -->
+                        <div class="total">
+                            <h6 class="text-dark">Subtotal
+                                <span class="text-dark ml-4">Rp. 889.500</span>
+                            </h6>
+                            <h6 class="text-dark">Shipping
+                                <span class="text-dark ml-4">Rp. 150.500</span>
+                            </h6>
+                        </div> <!-- end address-checkout -->
+                    </div> <!-- end foot-item -->
+                    <div class="card-total shadow p-3 mb-5 bg-white rounded">
+                        <div class="d-flex row">
+                            <h6 class="m-2">Total Amount</h6>
+                            <h5 class="m-2"><b>Rp. 1.235.500</b></h5>
+                        </div>
+                        <button class="btn btn-brown m-2">PROSES TO CHECKOUT</button>
+                    </div>
                 </div> <!-- end chart-box -->
             </div> <!-- end container -->
         </div> <!-- end content -->
